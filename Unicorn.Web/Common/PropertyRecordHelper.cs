39409c221b5c47c18c189f24e5d746a3632053b7
Stephen Liedig <sliedig@amazon.com>
2023-09-06 08:09:49 +0800

    feat: updated implementation for web. Needs a little more work.

--- Unicorn.Web/Common/PropertyRecordHelper.cs
+++ Unicorn.Web/Common/PropertyRecordHelper.cs
@@ -8,17 +8,6 @@ namespace Unicorn.Web.Common;
 /// </summary>
 public static class PropertyRecordHelper
 {
-    public static string GetPartitionKey(string country, string city)
-    {
-        var pkDetails = $"{country}#{city}".Replace(' ', '-').ToLower();
-        return $"property#{pkDetails}";
-    }
-
-    public static string GetSortKey(string street, string propertyNumber)
-    {
-        return $"{street}#{propertyNumber}".Replace(' ', '-').ToLower();
-    }
-
     public static PropertyDto ToDto(PropertyRecord property)
     {
         return new PropertyDto
@@ -34,5 +23,39 @@ public static class PropertyRecordHelper
             Images = property.Images,
             Status = property.Status
         };
+    }
+    
+    
+    /// <summary>
+    /// Extract the parts of the Property ID to get the PK and SK
+    /// components for a table query
+    /// </summary>
+    /// <param name="propertyId">Property ID</param>
+    /// <returns>Dictionary of PK and SK </returns>
+    public static Dictionary<string, string> ParsePropertyId(string propertyId)
+    {
+        var splitString = propertyId.Split('/');
+        var country = splitString[0];
+        var city = splitString[1];
+        var street = splitString[2];
+        var number = splitString[3];
+
+        var keys = new Dictionary<string, string>()
+        {
+            { "pk", GetPartitionKey(country, city) },
+            { "sk", GetSortKey(street, number) }
+        };
+
+        return keys;
+    }
+    public static string GetPartitionKey(string country, string city)
+    {
+        var pkDetails = $"{country}#{city}".Replace(' ', '-').ToLower();
+        return $"property#{pkDetails}";
+    }
+    
+    public static string GetSortKey(string street, string propertyNumber)
+    {
+        return $"{street}#{propertyNumber}".Replace(' ', '-').ToLower();
     }
 }
